71cc836f84c2ff6f7d4afba06cfb9f6816631d79
bohdandrychak <bohdan.drychak-ki2101@kep.nung.edu.ua>
2024-11-22 02:47:37 +0200

    Add files via upload

---
+++ PR9/script.js
@@ -0,0 +1,77 @@
+var swiper = new Swiper(".swiper", {
+    slidesPerView: 4,
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      breakpoints: {
+        1080: {
+            slidesPerView: 4,
+        },
+        900: {
+            slidesPerView: 3
+        },
+        700: {
+            slidesPerView: 2,
+        },
+        0: {
+            slidesPerView: 1,
+        }
+    }
+});
+
+let transaltions = {}
+const swiper_wrapper =  document.getElementById('swiper-wrapper')
+async function loadData(lang){
+    try{
+        const response = await fetch(`https://bohdandrychak.github.io//PR9/${lang}.json`)
+        transaltions = await response.json()
+        createSlides(transaltions)
+    }
+    catch(error){
+        console.log(`Щось пішло не так: ${error}`)
+    }
+}
+
+function createSlides(data){
+    swiper_wrapper.innerHTML = ''
+    data.products.forEach(product => {
+        let slide = document.createElement('div');
+        let productTag = document.createElement('div');
+        let category = document.createElement('div');
+        let image = document.createElement('img');
+        let name = document.createElement('a');
+        let price = document.createElement('div'); 
+        let btn = document.createElement('button');
+        
+        slide.className = "swiper-slide"
+        if(product.productTag != 'none'){
+            productTag.className = "product"
+            productTag.classList.add(product.productTagClass)
+        }
+        category.className = "product-category"
+        category.textContent = product.category
+        image.src = `images/${product.image}`
+        name.textContent = product.name
+        name.className = "product-name"
+        name.href = product.link
+        btn.className = "Btn"
+        if (product.price != "none"){
+           price.textContent = product.price
+           btn.classList.add("buyBtn")
+        }
+        else{
+            btn.classList.add("noBtn")
+        }
+        btn.textContent = product.available
+        slide.appendChild(productTag)
+        slide.appendChild(category)
+        slide.appendChild(image)
+        slide.appendChild(name)
+        slide.appendChild(price)
+        slide.appendChild(btn)
+        swiper_wrapper.appendChild(slide)
+    });
+}
+loadData('en')
